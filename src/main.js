42c8ed1701136d61385d5bd3a8e1aaa6ca78a1c6
longma980518 <longma980518@163.com>
2020-12-28 15:22:11 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -8,8 +8,14 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 
 import axios from 'axios'
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
